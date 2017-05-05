1bccc2e5053a21fc6eb71decc8bf6535bdfe2997
Gaceusz <inglorion@op.pl>
2017-05-05 15:04:47 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var a = prompt('Wpisz długość podstawy trójkąta');
 var h = prompt('Wpisz długość wysokości trójkąta');
 
-var triangleArea =a*h/2;
+var triangleArea = a*h/2;
 
 console.log('Pole trojkata o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 
